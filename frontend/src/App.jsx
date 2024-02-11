e7d53a00fd7b64e8b074daa2e93646aa410501a6
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-02-11 23:35:06 +0530

    login signup ui and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/login/login'
+import { Signup } from './pages/signup/Signup'
 function App() {
   
 
   return (
     <>
       <div className='p-4 h-screen flex items-center justify-center'>
-        <Login/>
+        
+        <Routes>
+        <Route path='/login' element={ <Login />} />
+				<Route path='/signup' element={<Signup />} />
+        </Routes>
       </div>
     </>
   )
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from "react-router-dom";
 const Login = ()  =>{
   return (
-    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter
-        backdrop-blur-lg bg-opacity-0'>
-            <h1>login</h1>
+    <div className='flex flex-col items-center justify-center w-screen h-auto mx-auto'>
+        <div className='h-full w-1/3 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border flex flex-col items-center justify-center border-gray-100 '>
+            <h1 className="text-2xl font-bold flex items-center justify-around ">Login   &nbsp; &nbsp; &nbsp;
+            <span className='text-blue-500'>  ChatApp</span></h1>
+            <form>
+                <div className='p-3 w-50'>
+                    <label className='label'>
+                        <span className='text-base leabel-text'>Username:</span>
+                    </label>
+                    <input
+							type='text'
+							placeholder='Enter username'
+							className='input input-bordered input-black w-full max-w-xs  ' />
+                </div>
+                <div className='p-3'>
+						<label className='label'>
+							<span className='text-base label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='input input-bordered input-black w-full max-w-xs'/>
+
+                </div>
+                    
+
+                <div className='flex flex-col items-center justify-between p-3  w-full'>
+                    <button className="btn btn-success w-5/6">LOG IN!</button>
+                    <Link to='/signup'> <h3 className='text-sm hover:underline'>HAVEN'T SIGNED UP YET?</h3></Link>
+                </div>
+            </form>
         </div>
+'
     </div>
   )
 }
