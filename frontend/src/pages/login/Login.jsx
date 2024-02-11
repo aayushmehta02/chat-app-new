c421f542e545de7eb6de125917af3fbb2dee6448
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-02-11 15:54:44 +0530

    frontend started

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = ()  =>{
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter
+        backdrop-blur-lg bg-opacity-0'>
+            <h1>login</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Login
