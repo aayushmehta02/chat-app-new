5273debb357f887cc4999415cedcece5932b4d97
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-02-12 23:27:23 +0530

    added new files

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<MessageContainer />
+	</div>
   )
 }
